33d9e7582f9f23240b2d8bcfe2ab6ca9e839901e
HillaryOtien <hillaryachayo@gmail.com>
2023-10-09 17:55:41 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -A program that prints a line with puts function
+ * Return: always (o)
+ */
+int main(void)
+{		
+puts("\"Programming is like building a multilingual puzzle\n");
+return (o);
+}
